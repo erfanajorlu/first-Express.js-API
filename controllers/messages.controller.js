a75c928b9a68ea69c7d6d42302c87f5187ff9354
Erfan Ajorlu <erfanajoglu098@gmail.com>
2023-12-27 15:08:32 +0330

    Templating engines

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,8 +1,10 @@
 const path = require('path');
 
 function getMessage(req, res) {
-  res.sendFile(path.join(__dirname , '..' , 'public' , 'skimountain.jpg'));
-}
+  res.render('messages', {
+    title: 'Messages to my Friends!',
+    friend: 'Elon Musk',
+  })}
 
 function postMessage(req, res) {
   console.log("Updating messages ...");
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const path = require("path");
 
 const messagesRouter = require("./routes/messages.router");
-const friendRouter = require('./routes/friends.router');
+const friendRouter = require("./routes/friends.router");
 
 const PORT = 3000;
 
 const app = express();
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use("/site", express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
   const start = Date.now();
@@ -16,8 +22,15 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.use('/friends' , friendRouter);
-app.use('/messages' , messagesRouter);
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "My Friends Are VERYY Clever",
+    caption: "Let's go skiing!",
+  });
+});
+
+app.use("/friends", friendRouter);
+app.use("/messages", messagesRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
