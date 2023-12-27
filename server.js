128f1a4616faf508fab26ad3132a555ec5fdbbcc
Erfan Ajorlu <erfanajoglu098@gmail.com>
2023-12-27 10:54:34 +0330

    Route parameters

--- server.js
+++ server.js
@@ -6,12 +6,36 @@ const PORT = 3000;
 
 const app = express();
 
-app.get('/' , (req , res) => {
-    res.send({
+
+const friends = [
+    {
+        id : 0,
+        name : 'Albert Einsten'
+    }
+    ,{
         id : 1,
         name : "Sir Isaac Newton"
-    });
+    }
+];
+
+
+app.get('/friends' , (req , res) => {
+    res.status(200).json(friends);
 });
+
+
+app.get('/friends/:friendId' , (req , res) => {
+    const id = Number(req.params.friendId);
+    const friend = friends[id];
+    if(friend){
+        res.status(200).json(friend);
+    }else{
+        res.status(404).json({
+            error : "friend does not exist"
+        })
+    }
+});
+
 
 app.get('/messages' , (req , res) =>{
     res.send('<ul><li>Hello Albert</li></ul>')
