782cd7d373833bbdec84ecb526e2b07aaeb27a33
Erfan Ajorlu <erfanajoglu098@gmail.com>
2023-12-27 11:02:21 +0330

    writing my logging middleware

--- server.js
+++ server.js
@@ -1,50 +1,51 @@
-const express = require('express');
-
+const express = require("express");
 
 const PORT = 3000;
 
-
 const app = express();
 
-
 const friends = [
-    {
-        id : 0,
-        name : 'Albert Einsten'
-    }
-    ,{
-        id : 1,
-        name : "Sir Isaac Newton"
-    }
+  {
+    id: 0,
+    name: "Albert Einsten",
+  },
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
+  },
 ];
 
-
-app.get('/friends' , (req , res) => {
-    res.status(200).json(friends);
+app.use((req, res, next) => {
+    const start = Date.now();
+    next();
+    const delta = Date.now() - start;
+    console.log(`${req.url} ${req.method} ${delta}ms`);
 });
 
-
-app.get('/friends/:friendId' , (req , res) => {
-    const id = Number(req.params.friendId);
-    const friend = friends[id];
-    if(friend){
-        res.status(200).json(friend);
-    }else{
-        res.status(404).json({
-            error : "friend does not exist"
-        })
-    }
+app.get("/friends", (req, res) => {
+  res.status(200).json(friends);
 });
 
-
-app.get('/messages' , (req , res) =>{
-    res.send('<ul><li>Hello Albert</li></ul>')
+app.get("/friends/:friendId", (req, res) => {
+  const id = Number(req.params.friendId);
+  const friend = friends[id];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: "friend does not exist",
+    });
+  }
 });
 
-app.post('/messages' , (req , res) =>{
-    console.log('Updating messages ...');
-})
+app.get("/messages", (req, res) => {
+  res.send("<ul><li>Hello Albert</li></ul>");
+});
 
-app.listen(PORT , ()=>{
-    console.log(`Listening on ${PORT}...`);
-})
+app.post("/messages", (req, res) => {
+  console.log("Updating messages ...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}...`);
+});
